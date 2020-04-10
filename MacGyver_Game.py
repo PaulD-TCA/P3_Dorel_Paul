e79ad06ba1fc91c0310d96cd3f6b3610960cedcc
Dorel <dorel.paul@gmail.com>
2020-04-10 16:11:37 +0200

    Main loop done in an object

--- MacGyver_Game.py
+++ MacGyver_Game.py
@@ -5,7 +5,7 @@ import pygame
 import os
 
 #main modules import
-from MacGyverModules import class_MacGyver, class_GameW, class_Furnitures, class_GameOver, class_Maze, class_GameEvent
+from MacGyverModules import class_MacGyver, class_GameW, class_Furnitures, class_GameOver, class_Maze, class_GameEvent, class_Mainloop
 
 if __name__ == '__main__':
 	
@@ -20,22 +20,5 @@ if __name__ == '__main__':
 	gameover = class_GameOver.GameOver(gamedpy.mazedisplay)
 
 	#Main game loop
-	while gameevent.gameloop == True:
-
-		gamedpy.mazedisplay()#Maze picture creation with an assemble of game pictures
-		furniture.picking(macgyver.mgx,macgyver.mgy)#items picking with a comparison between Mac Gaver and item position
-		gamedpy.gamew.blit(furniture.needle,(furniture.needleposition))#needle display
-		gamedpy.gamew.blit(furniture.ether,(furniture.etherposition))#ether display
-		gamedpy.gamew.blit(furniture.pipe,(furniture.pipeposition))#pipe display
-		gamedpy.gamew.blit(furniture.syringe,(furniture.syringeposition))#syringe display
-		furniture.syringecrafting(gamedpy.gamew, gamedpy.inv)#syringe crafting with the 3 other idems (needle, ether and pipe)
-		gamedpy.gamew.blit(macgyver.mgpic,(macgyver.mgpicx + 6,macgyver.mgpicy))#Mac Gyver display and displacement
-		gamedpy.gamew.blit(gameover.keeper,(gameover.keeperposition))#keeper position
-		if gameover.endgame == 1:#maze display just after the end of the game
-			gamedpy.mazedisplay()
-		gameover.keeperneut(macgyver.mgx,macgyver.mgy,furniture.craftcompleting,gamedpy.gamew)#end screen display after the end of the game
-		pygame.display.flip()#pygame display
-		gameevent.getgameevent()#game event input (keyboard event and closing)
-		macgyver.move(gameevent.way)#Mac Gyver displacement management
-
-		continue
+	maingameloop = class_Mainloop.Mainloop()
+	maingameloop.gamemainloop(gameevent, macgyver, gamedpy, furniture, gameover)
